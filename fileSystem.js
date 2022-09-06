d2e3f09a86b22bacf32fa8dfedb483c3dfd775f6
AdrianMiftahulH <adrianmiftahul@gmail.com>
2022-09-06 18:57:40 +0700

    Membuat file terpisah antara fileSystem dan readline, membuat Comment

---
+++ fileSystem.js
@@ -0,0 +1,4 @@
+//Membuat variabel fs
+const fs = require('fs');
+
+fs.writeFileSync('test.text', 'Hello world secara sync');
--- readline.js
+++ readline.js
@@ -1,25 +1,27 @@
-//const fs = require('fs');
-
-// fs.writeFileSync('test.text', 'Hello world secara syncrounous');
-
-
-
-
 const readline = require('readline');
+
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
+// Memmbuat pertanyaan nama 
 rl.question('what is your name ? ', (name) => {
+    // Membuat Pertanyaan no telepon
     rl.question('no phone ? ', (noPhone) => {
+        // Membuat pertanyaan email
         rl.question('email ? ', (email) => {
-            console.log(`Thank You ${name},no phone : ${noPhone},email : ${email}`);
+            // Output ke terminal sesaui jawaban di pertanyan
+            console.log(`Thank You ${name}, no phone : ${noPhone}, email : ${email}`);
+            // Keluar dati rl
             rl.close();
         });
     });
 });
+
+
+
 // membuat cli kontak
 // tentang yasn
 
