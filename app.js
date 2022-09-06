d9761568f54549e22ecc73ff10cf5cfe61c37033
AdrianMiftahulH <adrianmiftahul@gmail.com>
2022-09-06 15:55:18 +0700

    awalan node.js

---
+++ app.js
@@ -0,0 +1,27 @@
+//const fs = require('fs');
+
+// fs.writeFileSync('test.text', 'Hello world secara syncrounous');
+
+
+
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('what is your name ? ', (name) => {
+    rl.question('no phone ? ', (noPhone) => {
+        rl.question('email ? ', (email) => {
+            console.log(`
+Thank You ${name},
+no phone : ${noPhone},
+email : ${email}`
+            );
+            rl.close();
+        });
+    });
+});
+
